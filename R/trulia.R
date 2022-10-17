0711c7121404409920803bf661fd56d88fa3c1c4
brendan <brendan@mailo.com>
2022-10-17 11:05:33 -0400

    Added dependency
    Add a function to check dependencies and now writes to csv.

--- R/trulia.R
+++ R/trulia.R
@@ -1,4 +1,21 @@
+check_pkg_deps <- function() {
+  if(!require(tidyverse)) {
+    message("loading the tidyverse package")
+    library(tidyverse)
+  }
+  if(!require(rvest)) {
+    message("loading the rvest package")
+    library(rvest)
+  }
+  if(!require(polite)) {
+    message("loading the polite package")
+    library(polite)
+  }
+}
+
 trulia <- function(state, city, k) {
+  # to check for package dependencies
+  check_pkg_deps()
   prop <- data.frame()
   # vacant lots are given /home/ in the url
   for (i in 1:k) {
@@ -28,25 +45,24 @@ trulia <- function(state, city, k) {
         html_text2())
 
     features <- data.frame(x = str_subset(int_step$x, ": ")) %>%
-      separate(x, c("y", "z"), sep = ":") %>%
+      separate(x, c("y", "z"), sep = ": ") %>%
       pivot_wider(names_from = y, values_from = z) %>%
-      filter(`Property Type` == " Residential")
+      filter(`Property Type` == "Residential")
 
     location <- data.frame(x = scrape(alt_session) %>%
                              html_elements("img") %>%
                              html_attr("srcset")) %>%
-      filter(!is.na(x), str_detect(x,
-                                   "googleapis.com"))
+      filter(!is.na(x), str_detect(x, "googleapis.com"))
 
-    coord_step <- unlist(mutate(location, x = str_extract_all(x,
-                                                              "[:digit:]{2}.[:digit:]{14}")))
+    coord_step <- unlist(mutate(location,
+      x = str_extract_all(x, "[:digit:]{2}.[:digit:]{14}")))
 
     lat <- as.double(coord_step[1])
     lon <- -as.double(coord_step[2])
     coord <- data.frame(lat, lon)
 
     desc <- data.frame(desc = scrape(alt_session) %>%
-                         html_element(".dRixxa") %>%
+                         html_element(".GoKXY") %>%
                          html_text2())
 
     contents <- tibble(features, coord, desc) %>%
@@ -55,5 +71,7 @@ trulia <- function(state, city, k) {
     trulia_list <- bind_rows(trulia_list, contents) %>%
       janitor::clean_names()
   }
-  return(trulia_list)
+  write_csv(trulia_list, file = paste0(paste(
+    "Trulia-listings", city, state, Sys.Date(), sep = "-"), ".csv")
+    )
 }
